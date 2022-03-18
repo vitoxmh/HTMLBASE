15160c39d9dfd1d80397b623f110b13861e8f68d
Victor Morales <victor.morales@fusiona.cl>
2022-03-18 20:11:49 -0300

    ejemplo de check o radio

--- assets/js/src/index.ts
+++ assets/js/src/index.ts
@@ -1,1 +1,32 @@
-alert("Holaaaaaaaaaa");
+const cSelect = document.querySelectorAll('.c-select__selection');
+
+cSelect.forEach((element:any) => {
+
+
+    element?.addEventListener('click', (e: Event) => {
+
+        element.closest(".c-select__container").classList.add("c-select__display");
+    
+    });
+
+});
+
+const cSelectOptions = document.querySelectorAll('.c-select__options li');
+
+cSelectOptions.forEach((element:any) => {
+
+
+    element?.addEventListener('click', (e: Event) => {
+
+    
+        element.closest(".c-select").querySelector('input[type-ui="select"]').setAttribute("value", element.getAttribute('value')); 
+        element.closest(".c-select__container").classList.remove('c-select__display');
+        element.closest(".c-select").querySelector('.c-select__selection').textContent = element.textContent;
+    
+    });
+
+
+
+});
+
+
